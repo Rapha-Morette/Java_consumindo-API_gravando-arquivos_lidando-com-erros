ac2256d8d99b64440b5fdd4f43a89908def3b5f7
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-06-23 16:44:00 -0300

    Módulo 4 aula 2 concluído

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -43,6 +44,9 @@ public class PrincipalComListas {
 
         System.out.println("Lista de titulo ordenados");
         Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por Ano");
         System.out.println(lista);
     }
 }

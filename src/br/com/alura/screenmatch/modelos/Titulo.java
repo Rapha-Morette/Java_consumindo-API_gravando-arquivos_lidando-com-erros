2c00e090d5ce6ca70d5383356f4bf2a17c53db8f
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-06-28 15:10:17 -0300

    Modulo 4 concluido

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -83,7 +83,7 @@ public class Titulo implements Comparable<Titulo>{
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento + ", " + "Duração: " + duracaoEmMinutos;
+        return "(nome='" + nome +
+                ", anoDeLancamento= " + anoDeLancamento + ", " + "Duração= " + duracaoEmMinutos + ")";
     }
 }
